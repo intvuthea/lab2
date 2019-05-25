489bd23cd1e623f44286b642b3f8c06b24fd5f48
intvuthea <intvuthea007@gmail.com>
2019-05-25 19:39:14 +0700

    edit1

--- fibonaci.php
+++ fibonaci.php
@@ -1,16 +1,39 @@
 <?php
-	echo date("h-m-s")."<br>";
+	
 	function fibonaci($n){
-		if($n>2){
+		if($n>=2){
 			return fibonaci($n-2)+fibonaci($n-1);
 		}
-		elseif ($n<=2){
+		elseif ($n==1){
 			return 1;
 		}
+		elseif ($n==0){
+			return 0;
+		}
 	}
-	echo fibonaci(6)."<br>";
-	echo date("h-m-s")."<br>";
+	echo microtime()."<br>";
+	echo fibonaci(8)."<br>";
+	echo microtime()."<br>";
 	function fibo($n){
-		
+		$n0=0;
+		$n1=1;
+		if($n==0){
+			return $n0;
+		}
+		else if($n==1){
+			return $n1;
+		}
+		else if ($n>=2){
+			for ($i=1;$i<$n;$i++){
+				$n2=$n1+$n0;
+				$n1=$n2;
+				$n0=$n2-$n0;
+			}
+			return $n2;
+		}	
 	}
-?>
+	echo microtime()."<br>";
+	echo fibo(8)."<br>";
+	echo microtime()."<br>";
+
+	?>
